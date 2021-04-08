3caa539749dbf6456b72e22071de3fabbc4343af
MagKaur <kochanowska.magd@gmail.com>
2021-04-08 20:54:00 +0200

    Added persistence.xml,Entity

---
+++ src/main/java/main.java
@@ -0,0 +1,15 @@
+import javax.persistence.*;
+
+public class main {
+
+    public static void main(String[] args){
+
+        EntityManagerFactory entityManagerFactory =
+
+        test_aeh_students test = new test_aeh_students();
+
+
+        test.getName();
+        test.getSurname();
+    }
+}
---
+++ src/main/java/test_aeh_students.java
@@ -0,0 +1,47 @@
+import javax.persistence.*;
+
+
+@Entity
+@Table (name = "TEST_AEH_STUDENTS")
+public class test_aeh_students {
+
+    @Id
+    @Column (name = "test_student_id")
+    @GeneratedValue(generator = "incrementor")
+    private int id;
+
+    @Column (name = "name")
+    private String name;
+
+    @Column (name = "surname")
+    private String surname;
+
+
+    public test_aeh_students() {}
+
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+}
