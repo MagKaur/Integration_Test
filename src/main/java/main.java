9609456a37127ed3a6536c13095ec61982e4b003
jpraj <j.prajzendanc@gmail.com>
2021-04-10 17:04:16 +0200

    pobierz dane z bazy danych

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -8,23 +8,13 @@ class integration_test {
 
         test_aeh_students student = new test_aeh_students();
         studentService service = new studentService(student);
-        //Searching for student by name
-        /*Scanner sc = new Scanner(System.in);
-        System.out.println("Enter name: ");
-        getStudent s = new getStudent(sc.nextLine());
-        s.getStudentVariable()*/
 
-        //Adding student to database
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter name and surname: ");
         String nazwaWpisana = scanner.nextLine();
         service.przypiszDane(nazwaWpisana);
+        //Adding student to database
         service.addStudent();
-
-        //addStudent add = new addStudent(a.nextLine());
-        //add.assign();
-        //add.addStudent();
-
-
+        service.getStudent();
     }
 }
